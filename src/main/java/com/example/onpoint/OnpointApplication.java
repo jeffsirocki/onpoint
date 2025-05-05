d16260505b765ed848faa8114975efa44cf22f88
jeffsirocki <jsirocki@gmail.com>
2025-05-05 07:43:04 -0400

    initiate spring boot app with gradle (jvm 23.0.2) and docker

---
+++ src/main/java/com/example/onpoint/OnpointApplication.java
@@ -0,0 +1,16 @@
+package com.example.onpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnpointApplication {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello Onpoint!\n");
+        SpringApplication.run(OnpointApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/onpoint/OnpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
